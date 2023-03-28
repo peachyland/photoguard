60a78bf6bb02e5f670b75edd0b7a5300042459b2
renjie3 <thurenjie@foxmail.com>
2023-03-27 23:24:28 -0400

    square

--- notebooks/sqaure_attack_inpainting.py
+++ notebooks/sqaure_attack_inpainting.py
@@ -1,6 +1,6 @@
 
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 from PIL import Image, ImageOps
 import requests
 import torch
@@ -14,10 +14,21 @@ from io import BytesIO
 from diffusers import StableDiffusionInpaintPipeline
 import torchvision.transforms as T
 from typing import Union, List, Optional, Callable
-from torchvision.utils import save_image
 
 from utils import preprocess, prepare_mask_and_masked_image, recover_image, prepare_image
 to_pil = T.ToPILImage()
+
+from square import SquareAttack
+
+import argparse
+# General Options
+parser = argparse.ArgumentParser(description='ClasswiseNoise')
+parser.add_argument('--input_latent', action='store_true', default=False)
+parser.add_argument('--output_latent', action='store_true', default=False)
+parser.add_argument("--num_inference_steps", type=int, default=100, required=False)
+parser.add_argument("--job_id", type=str, default='local', required=False)
+
+args = parser.parse_args()
 
 # make sure you're logged in with `huggingface-cli login` - check https://github.com/huggingface/diffusers for more details
 
@@ -44,197 +55,140 @@ target_image = target_image.resize((512, 512))
 target_image
 
 # A differentiable version of the forward function of the inpainting stable diffusion model! See https://github.com/huggingface/diffusers
-def attack_forward(
+class AttackForward():
+    def __init__(
         self,
+        inpaint_pipeline,
         prompt: Union[str, List[str]],
-        masked_image: Union[torch.FloatTensor, Image.Image],
         mask: Union[torch.FloatTensor, Image.Image],
-        count = 0,
+        target_image = None,
         height: int = 512,
         width: int = 512,
         num_inference_steps: int = 50,
         guidance_scale: float = 7.5,
         eta: float = 0.0,
+        input_latent = True,
+        output_latent = True,
+    ):
+        self.inpaint_pipeline = inpaint_pipeline
+        self.prompt = prompt
+        self.mask = mask
+        self.height = height
+        self.width = width
+        self.num_inference_steps = num_inference_steps
+        self.guidance_scale = guidance_scale
+        self.eta = eta
+        self.target_image = target_image.to(torch.float16).cuda().unsqueeze(0)
+        if input_latent:
+            self.latent_mask = torch.nn.functional.interpolate(mask, size=(height // 8, width // 8))
+        else:
+            self.latent_mask = mask
+        self.input_latent = input_latent
+        self.output_latent = output_latent
+
+    def __call__(self,
+        masked_image_latents: Union[torch.FloatTensor, Image.Image],
     ):
 
-        text_inputs = self.tokenizer(
+        prompt = self.prompt
+        mask = self.mask
+        height = self.height
+        width = self.width
+        num_inference_steps = self.num_inference_steps
+        guidance_scale = self.guidance_scale
+        eta = self.eta
+
+        text_inputs = self.inpaint_pipeline.tokenizer(
             prompt,
             padding="max_length",
-            max_length=self.tokenizer.model_max_length,
+            max_length=self.inpaint_pipeline.tokenizer.model_max_length,
             return_tensors="pt",
         )
         text_input_ids = text_inputs.input_ids
-        text_embeddings = self.text_encoder(text_input_ids.to(self.device))[0]
+        text_embeddings = self.inpaint_pipeline.text_encoder(text_input_ids.to(self.inpaint_pipeline.device))[0]
 
         uncond_tokens = [""]
         max_length = text_input_ids.shape[-1]
-        uncond_input = self.tokenizer(
+        uncond_input = self.inpaint_pipeline.tokenizer(
             uncond_tokens,
             padding="max_length",
             max_length=max_length,
             truncation=True,
             return_tensors="pt",
         )
-        uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+        uncond_embeddings = self.inpaint_pipeline.text_encoder(uncond_input.input_ids.to(self.inpaint_pipeline.device))[0]
         seq_len = uncond_embeddings.shape[1]
         text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
         
         text_embeddings = text_embeddings.detach()
 
-        num_channels_latents = self.vae.config.latent_channels
+        num_channels_latents = self.inpaint_pipeline.vae.config.latent_channels
         
         latents_shape = (1 , num_channels_latents, height // 8, width // 8)
-        latents = torch.randn(latents_shape, device=self.device, dtype=text_embeddings.dtype)
+        latents = torch.randn(latents_shape, device=self.inpaint_pipeline.device, dtype=text_embeddings.dtype)
 
         mask = torch.nn.functional.interpolate(mask, size=(height // 8, width // 8))
         mask = torch.cat([mask] * 2)
 
-        masked_image_latents = self.vae.encode(masked_image).latent_dist.sample()
+        if not self.input_latent:
+            masked_image_latents = self.inpaint_pipeline.vae.encode(masked_image_latents.to(torch.float16)).latent_dist.sample()
         masked_image_latents = 0.18215 * masked_image_latents
         masked_image_latents = torch.cat([masked_image_latents] * 2)
 
-        latents = latents * self.scheduler.init_noise_sigma
-        
-        self.scheduler.set_timesteps(num_inference_steps)
-        timesteps_tensor = self.scheduler.timesteps.to(self.device)
+        # print(self.target_image.shape)
 
-        for j in range(masked_image_latents.shape[0]):
-            save_img = masked_image_latents[j, :3]
-            # print(save_img.shape)
-            save_img = (save_img - torch.min(save_img)) / (torch.max(save_img) - torch.min(save_img))
-            save_image(save_img, './save_img_output/input_latents{}_{}.png'.format(j, count))
-            break
+        target_image_latents = self.inpaint_pipeline.vae.encode(self.target_image).latent_dist.sample()
+        # target_image_latents = 0.18215 * target_image_latents
+
+        # from torchvision.utils import save_image
+        # for j in range(target_image_latents.shape[0]):
+        #     save_img = target_image_latents[j, :3]
+        #     print(save_img.shape)
+        #     save_img = (save_img - torch.min(save_img)) / (torch.max(save_img) - torch.min(save_img))
+        #     save_image(save_img, './target_image_latents{}.png'.format(j))
+
+        latents = latents * self.inpaint_pipeline.scheduler.init_noise_sigma
+        
+        self.inpaint_pipeline.scheduler.set_timesteps(num_inference_steps)
+        timesteps_tensor = self.inpaint_pipeline.scheduler.timesteps.to(self.inpaint_pipeline.device)
 
         for i, t in enumerate(timesteps_tensor):
             latent_model_input = torch.cat([latents] * 2)
-            latent_model_input = torch.cat([latent_model_input, mask, masked_image_latents], dim=1)
-            noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+            latent_model_input = torch.cat([latent_model_input.to(torch.float16), mask, masked_image_latents], dim=1)
+            noise_pred = self.inpaint_pipeline.unet(latent_model_input.to(torch.float16), t, encoder_hidden_states=text_embeddings).sample
             noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
             noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-            latents = self.scheduler.step(noise_pred, t, latents, eta=eta).prev_sample
-
-        for j in range(latents.shape[0]):
-            save_img = latents[j, :3]
-            # print(save_img.shape)
-            save_img = (save_img - torch.min(save_img)) / (torch.max(save_img) - torch.min(save_img))
-            save_image(save_img, './save_img_output/output_latents{}_{}.png'.format(j, count))
-            break
+            latents = self.inpaint_pipeline.scheduler.step(noise_pred, t, latents, eta=eta).prev_sample
 
         latents = 1 / 0.18215 * latents
-        image = self.vae.decode(latents).sample
-        return image
-
-
-# A differentiable version of the forward function of the inpainting stable diffusion model! See https://github.com/huggingface/diffusers
-def pipeline_forward(
-        self,
-        prompt: Union[str, List[str]],
-        masked_image: Union[torch.FloatTensor, Image.Image],
-        mask: Union[torch.FloatTensor, Image.Image],
-        count = 0,
-        height: int = 512,
-        width: int = 512,
-        num_inference_steps: int = 50,
-        guidance_scale: float = 7.5,
-        eta: float = 0.0,
-    ):
-
-        text_inputs = self.tokenizer(
-            prompt,
-            padding="max_length",
-            max_length=self.tokenizer.model_max_length,
-            return_tensors="pt",
-        )
-        text_input_ids = text_inputs.input_ids
-        text_embeddings = self.text_encoder(text_input_ids.to(self.device))[0]
-
-        uncond_tokens = [""]
-        max_length = text_input_ids.shape[-1]
-        uncond_input = self.tokenizer(
-            uncond_tokens,
-            padding="max_length",
-            max_length=max_length,
-            truncation=True,
-            return_tensors="pt",
-        )
-        uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
-        seq_len = uncond_embeddings.shape[1]
-        text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
-        
-        text_embeddings = text_embeddings.detach()
-
-        num_channels_latents = self.vae.config.latent_channels
-        
-        latents_shape = (1 , num_channels_latents, height // 8, width // 8)
-        latents = torch.randn(latents_shape, device=self.device, dtype=text_embeddings.dtype)
-
-        mask = torch.nn.functional.interpolate(mask, size=(height // 8, width // 8))
-        mask = torch.cat([mask] * 2)
-
-        masked_image_latents = self.vae.encode(masked_image).latent_dist.sample()
-        masked_image_latents = 0.18215 * masked_image_latents
-        masked_image_latents = torch.cat([masked_image_latents] * 2)
-
-        latents = latents * self.scheduler.init_noise_sigma
-        
-        self.scheduler.set_timesteps(num_inference_steps)
-        timesteps_tensor = self.scheduler.timesteps.to(self.device)
-
-        for j in range(masked_image_latents.shape[0]):
-            save_img = masked_image_latents[j, :3]
-            # print(save_img.shape)
-            save_img = (save_img - torch.min(save_img)) / (torch.max(save_img) - torch.min(save_img))
-            save_image(save_img, './save_img_output/input_latents{}_final.png'.format(j))
-            break
-
-        for i, t in enumerate(timesteps_tensor):
-            latent_model_input = torch.cat([latents] * 2)
-            latent_model_input = torch.cat([latent_model_input, mask, masked_image_latents], dim=1)
-            noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
-            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-            latents = self.scheduler.step(noise_pred, t, latents, eta=eta).prev_sample
-
-        for j in range(latents.shape[0]):
-            save_img = latents[j, :3]
-            # print(save_img.shape)
-            save_img = (save_img - torch.min(save_img)) / (torch.max(save_img) - torch.min(save_img))
-            save_image(save_img, './save_img_output/output_latents{}_final.png'.format(j))
-            break
-
-        latents = 1 / 0.18215 * latents
-        image = self.vae.decode(latents).sample
-        return image
+        if not self.output_latent:
+            image = self.inpaint_pipeline.vae.decode(latents).sample
+            return image, self.target_image
+        else:
+            return latents, target_image_latents
 
     
-def compute_grad(cur_mask, cur_masked_image, prompt, count, target_image, **kwargs):
-    torch.set_grad_enabled(True)
-    cur_mask = cur_mask.clone()
-    cur_masked_image = cur_masked_image.clone()
-    cur_mask.requires_grad = False
-    cur_masked_image.requires_grad_()
-    image_nat = attack_forward(pipe_inpaint,mask=cur_mask,count=count,
-                               masked_image=cur_masked_image,
-                               prompt=prompt,
-                               **kwargs)
-    
-    loss = (image_nat - target_image).norm(p=2)
-    grad = torch.autograd.grad(loss, [cur_masked_image])[0] * (1 - cur_mask)
-        
-    return grad, loss.item(), image_nat.data.cpu()
-
-def forward(cur_mask, cur_masked_image, prompt, count, **kwargs):
+def compute_grad(cur_mask, cur_masked_image, prompt, target_image, **kwargs):
     torch.set_grad_enabled(False)
     cur_mask = cur_mask.clone()
     cur_masked_image = cur_masked_image.clone()
     cur_mask.requires_grad = False
     cur_masked_image.requires_grad = False
-    image_nat = pipeline_forward(pipe_inpaint,mask=cur_mask,count=count,
-                               masked_image=cur_masked_image,
-                               prompt=prompt,
-                               **kwargs)
+    cur_masked_image_latents = pipe_inpaint.vae.encode(cur_masked_image).latent_dist.sample()
+    # print(cur_masked_image.shape)
+    attack_forward = AttackForward(pipe_inpaint, mask=cur_mask, prompt=prompt, target_image=target_image, input_latent=args.input_latent, output_latent=args.output_latent, **kwargs)
+    square_attack = SquareAttack(attack_forward, p_init=.8, n_queries=5000, eps=5.5, norm='Linf', n_restarts=1, seed=1, verbose=False, device='cuda', resc_schedule=False)
+    if args.input_latent:
+        adv_latents = square_attack.perturb(x=cur_masked_image_latents,)
+        adv_x = pipe_inpaint.vae.decode(adv_latents.half()).sample
+    else:
+        adv_x = square_attack.perturb(x=cur_masked_image.half(),)
+    
+    # loss = (image_nat - target_image).norm(p=2)
+
+    # latents = 1 / 0.18215 * latents
         
-    return image_nat.data.cpu()
+    return adv_x
 
 def super_l2(cur_mask, X, prompt, step_size, iters, eps, clamp_min, clamp_max, grad_reps = 5, target_image = 0, **kwargs):
     X_adv = X.clone()
@@ -269,29 +223,11 @@ def super_l2(cur_mask, X, prompt, step_size, iters, eps, clamp_min, clamp_max, g
 
 def super_linf(cur_mask, X, prompt, step_size, iters, eps, clamp_min, clamp_max, grad_reps = 5, target_image = 0, **kwargs):
     X_adv = X.clone()
-    iterator = tqdm(range(iters))
-    for i in iterator:
-
-        all_grads = []
-        losses = []
-        for _ in range(grad_reps):
-            c_grad, loss, last_image = compute_grad(cur_mask, X_adv, prompt, count = i, target_image=target_image, **kwargs)
-            all_grads.append(c_grad)
-            losses.append(loss)
-        grad = torch.stack(all_grads).mean(0)
-        
-        iterator.set_description_str(f'AVG Loss: {np.mean(losses):.3f}')
-        
-        # actual_step_size = step_size - (step_size - step_size / 100) / iters * i
-        actual_step_size = step_size
-        X_adv = X_adv - grad.detach().sign() * actual_step_size
-
-        X_adv = torch.minimum(torch.maximum(X_adv, X - eps), X + eps)
-        X_adv.data = torch.clamp(X_adv, min=clamp_min, max=clamp_max)
+    adv_x = compute_grad(cur_mask, X_adv, prompt, target_image=target_image, **kwargs)
         
     torch.cuda.empty_cache()
 
-    return X_adv, last_image
+    return X_adv
 
 prompt = ""
 SEED = 786349
@@ -299,7 +235,7 @@ torch.manual_seed(SEED)
 
 strength = 0.7
 guidance_scale = 7.5
-num_inference_steps = 4
+num_inference_steps = args.num_inference_steps
 
 cur_mask, cur_masked_image = prepare_mask_and_masked_image(init_image, mask_image)
 
@@ -325,10 +261,10 @@ target_image_tensor = 0*target_image_tensor.cuda() # we can either attack toward
 # Alternatively you can run an l_inf pgd attack
 print(torch.min(cur_masked_image))
 print(torch.max(cur_masked_image))
-result, last_image= super_linf(cur_mask, cur_masked_image,
+result = super_linf(cur_mask, cur_masked_image,
                   prompt=prompt,
                   target_image=target_image_tensor,
-                  eps=0.1,
+                  eps=0.0628,
                   step_size=0.006,
                   iters=200,
                   clamp_min = -1,
@@ -386,22 +322,6 @@ image_adv = pipe_inpaint(prompt=prompt,
                     #  strength=strength
                     ).images[0]
 image_adv = recover_image(image_adv, init_image, mask_image)
-
-cur_mask, cur_masked_image = prepare_mask_and_masked_image(adv_image, mask_image)
-
-cur_mask = cur_mask.half().cuda()
-cur_masked_image = cur_masked_image.half().cuda()
-
-print(type(adv_image))
-forward(cur_mask, cur_masked_image,
-                  prompt=prompt,
-                  count = 0,
-                  height = 512,
-                  width = 512,
-                  eta=1,
-                  num_inference_steps=num_inference_steps,
-                  guidance_scale=guidance_scale,
-                )
 
 fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(20,6))
 
